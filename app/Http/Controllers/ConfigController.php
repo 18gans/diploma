54d08cc4bdaa46a4d3fe86223591fa25da674cab
air <asilkhan-al@mail.ru>
2020-05-18 23:38:03 +0600

    Initial commit

--- app/Http/Controllers/ConfigController.php
+++ app/Http/Controllers/ConfigController.php
@@ -9,12 +9,13 @@
 namespace App\Http\Controllers;
 
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Artisan;
 
 class ConfigController extends Controller
 {
 
-    public function command($request)
+    public function command(Request $request)
     {
         if ($request->token == 'kasya') {
             return Artisan::call($request->command);
