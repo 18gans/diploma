c5fb4345312b73e87254210919edd84cc94e2cf0
18gans <65560403+18gans@users.noreply.github.com>
2020-06-10 01:09:03 +0600

    Update register.blade.php

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -5,14 +5,14 @@
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
-                    <div class="card-header">Регистрация</div>
+                    <div class="card-header">Тіркелу</div>
 
                     <div class="card-body">
                         <form method="POST" action="{{ route('register') }}">
                             @csrf
 
                             <div class="form-group row">
-                                <label for="name" class="col-md-4 col-form-label text-md-right">Имя</label>
+                                <label for="name" class="col-md-4 col-form-label text-md-right">Аты-жөні</label>
 
                                 <div class="col-md-6">
                                     <input id="name" type="text"
@@ -46,7 +46,7 @@
 
                             <div class="form-group row">
                                 <label for="password"
-                                       class="col-md-4 col-form-label text-md-right">Пароль</label>
+                                       class="col-md-4 col-form-label text-md-right">Құпия сөз</label>
 
                                 <div class="col-md-6">
                                     <input id="password" type="password"
@@ -63,7 +63,7 @@
 
                             <div class="form-group row">
                                 <label for="password-confirm"
-                                       class="col-md-4 col-form-label text-md-right">Подтвердите пароль</label>
+                                       class="col-md-4 col-form-label text-md-right">Құпия сөзді растау</label>
 
                                 <div class="col-md-6">
                                     <input id="password-confirm" type="password" class="form-control"
@@ -74,7 +74,7 @@
                             <div class="form-group row mb-0">
                                 <div class="col-md-6 offset-md-4">
                                     <button type="submit" class="btn btn-primary">
-                                        Регистрация
+                                        Тіркелу
                                     </button>
                                 </div>
                             </div>
