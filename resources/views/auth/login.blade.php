e82b487bf37d2a3e1b28486ba3f24834b937055f
18gans <65560403+18gans@users.noreply.github.com>
2020-06-10 01:05:10 +0600

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,7 +5,7 @@
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
-                    <div class="card-header">Вход</div>
+                    <div class="card-header">Кіру</div>
 
                     <div class="card-body">
                         <form method="POST" action="{{ route('login') }}">
@@ -52,7 +52,7 @@
                                                id="remember" {{ old('remember') ? 'checked' : '' }}>
 
                                         <label class="form-check-label" for="remember">
-                                            Запомнить меня
+                                            Логинді сақтау
                                         </label>
                                     </div>
                                 </div>
@@ -61,7 +61,7 @@
                             <div class="form-group row mb-0">
                                 <div class="col-md-8 offset-md-4">
                                     <button type="submit" class="btn btn-primary">
-                                        Вход
+                                       Кіру 
                                     </button>
                                 </div>
                             </div>
